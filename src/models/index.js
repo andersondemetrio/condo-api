ac1f684a6aebc861f2df60389abd4c2ad8817a57
andersondemetrio <anderson.debora@gmail.com>
2026-03-27 12:13:52 -0300

    fix: explicit pg injection for serverless

--- src/models/index.js
+++ src/models/index.js
@@ -4,10 +4,20 @@ const dbConfig = require('../config/database');
 const env = process.env.NODE_ENV || 'development';
 const config = dbConfig[env];
 
+/**
+ * AJUSTE PARA VERCEL:
+ * Injetamos o módulo 'pg' diretamente na configuração.
+ * Isso evita o erro "Please install pg package manually".
+ */
+if (config.dialect === 'postgres' || !config.dialect) {
+  config.dialectModule = require('pg');
+}
+
 const sequelize = new Sequelize(config.url, config);
 
 const db = {};
 
+// Importação dos modelos
 db.User = require('./User')(sequelize);
 db.Reservation = require('./Reservation')(sequelize);
 db.Holiday = require('./Holiday')(sequelize);
@@ -15,7 +25,7 @@ db.CondoItem = require('./CondoItem')(sequelize);
 db.ReservationItem = require('./ReservationItem')(sequelize);
 db.CheckoutForm = require('./CheckoutForm')(sequelize);
 
-// Run all associations
+// Executa as associações (relacionamentos) se existirem
 Object.values(db).forEach((model) => {
   if (typeof model.associate === 'function') {
     model.associate(db);
